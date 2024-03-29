c873d1dede30fe7880573bde97d8c602c021c70b
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-03-29 10:20:07 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,16 +7,16 @@ class Config(object):
     # sucks Dude
     APP_ID = 4018758  # Updated with your API ID
     API_HASH = "622bba3cf046315531f71f9d97fa6c2a"  # Updated with your API HASH
-    LOG_CHANNEL = "-1002108819224" # Updated with your log channel ID
+    LOG_CHANNEL = "-1001912703522" # Updated with your log channel ID
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
     # Get these values from my.telegram.org
-    AUTH_USERS = {6748415360}
+    AUTH_USERS = {6748415360 -1001912703522}
     # auth users jdk 
     TG_BOT_TOKEN = "6650630304:AAFqJ_xw_SEMydEYJJbWOmi3f89hdLJMgZ0"  # Updated with your bot token
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
-    BOT_USERNAME = "Bot144255262626_bot"  # Updated with your bot username
+    BOT_USERNAME = "OM_Compressor_Bot"  # Updated with your bot username
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 2097152000
